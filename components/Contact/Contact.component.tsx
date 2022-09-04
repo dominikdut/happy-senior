e4d29af310e497815b4a79637fe377feb0f1cb22
Dominik Dutkiewicz <d.dutkiewicz@tme.pl>
2022-09-04 14:16:03 +0200

    Fix for links in contact

--- components/Contact/Contact.component.tsx
+++ components/Contact/Contact.component.tsx
@@ -8,8 +8,10 @@ export const ContactComponent = (props) => {
         <div ref={contact} className={styles.contact__wrapper}>
             <div className={styles.contact__container}>
                 <h2>Kontakt</h2>
-                <a href={`tel:+48666666666`}>Telefon: +48 666 666 666</a>
-                <a href={`mailto:happyseniorresthome@gmail.com`}>Adres e-mail: happyseniorresthome@gmail.com</a>
+                <div className={styles.contact__linksContainer}>
+                    <a href={`tel:+48666666666`}>Telefon: +48 666 666 666</a>
+                    <a href={`mailto:happyseniorresthome@gmail.com`}>Adres e-mail: happyseniorresthome@gmail.com</a>
+                </div>
                 <iframe
                     className={styles.contact__iframe}
                     src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2332.121517833512!2d17.753619215871964!3d54.23103798017395!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46fdf591930ae061%3A0x107fcda4156b5958!2sHappy%20senior!5e0!3m2!1spl!2spl!4v1662225385031!5m2!1spl!2spl"
